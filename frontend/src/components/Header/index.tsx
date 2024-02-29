265053d2970e9516c6a872ff03ca1b8b34cbea51
Plinio Sena <pliniodasilvasena@gmail.com>
2024-02-28 21:15:26 -0300

    componente

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+function Header() {
+    return (
+        <header>
+            <div>
+                <span>Uma besteira qualquer</span>
+            </div>
+            <nav>
+                <a href="#">LINK 1</a>
+                <a href="#">LINK 2</a>
+                <a href="#">LINK 3</a>
+                <a href="#">LINK 4</a>
+            </nav>
+        </header>
+    )
+
+}
+
+export default Header;
