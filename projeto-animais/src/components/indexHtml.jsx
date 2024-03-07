bc2db1927b8f1e41bb03a2f69c9f92da395c438e
Luciana <luciana_mls12@hotmail.com>
2024-03-07 20:24:16 -0300

    Criando componente Animal

---
+++ projeto-animais/src/components/indexHtml.jsx
@@ -0,0 +1,461 @@
+
+import React from 'react';
+
+
+
+const MeuComponenteHTML = () => {
+  return (
+    <>
+        <span id="amarelotopo">.</span>
+        <span className="mt-5 mb-5">.</span>
+        <section id="titulo" className="mt-5">
+            <header className="row">
+                <h1 className="col-10 offset-1 text-center mt-5 p-4">Sons dos Animais</h1>
+            </header>
+        </section>
+        
+        
+        <section id="animais">
+            
+            <article className="animal amarelo" onClick={() => abelhabee2()}>
+                <img src={"./src/assets/imagens/animais/abelha-bee.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Abelha</h2>
+                <h3 className="m-0">Bee</h3>
+            </article>
+             
+
+            
+            <article className="animal rosa" onclick="alcemoose()">
+                <img src={"./src/assets/imagens/animais/alce-moose.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Alce</h2>
+                <h3 className="m-0">Moose</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="alpaca()">
+                <img src={"./src/assets/imagens/animais/alpaca.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Alpaca</h2>
+                <h3 className="m-0">Alpaca</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="antilopeantelope()">
+                <img src={"./src/assets/imagens/animais/antilope-antelope.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Antílope</h2>
+                <h3 className="m-0">Antelope</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="baleiadagroelandiabowheadwhale()">
+                <img src={"./src/assets/imagens/animais/baleia-da-groelandia-bowhead-whale.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Baleia</h2>
+                <h3 className="m-0">Bowhead Whale</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="baleiajubartehumpbackwhale()">
+                <img src={"./src/assets/imagens/animais/baleia-jubarte-humpback-whale.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Baleia Jubarte</h2>
+                <h3 className="m-0">Humpback Whale</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="borboletabutterfly()">
+                <img src={"./src/assets/imagens/animais/borboleta-butterfly.png"} className="d-block m-auto"/>
+                <h2 className="m-0">Borboleta</h2>
+                <h3 className="m-0">Butterfly</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="bufalobuffalo()">
+                <img src="../imagens/animais/bufalo-buffalo.png" className="d-block m-auto"/>
+                <h2 className="m-0">Bufalo</h2>
+                <h3 className="m-0">Buffalo</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="cachorrodog()">
+                <img src="../imagens/animais/cachorro-dog.png" className="d-block m-auto"/>
+                <h2 className="m-0">Cachorro</h2>
+                <h3 className="m-0">Dog</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="camelocamel()">
+                <img src="../imagens/animais/camelo-camel.png" className="d-block m-auto"/>
+                <h2 className="m-0">Camelo</h2>
+                <h3 className="m-0">Camel</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="cavalohorse()">
+                <img src="../imagens/animais/cavalo-horse.png" className="d-block m-auto"/>
+                <h2 className="m-0">Cavalo</h2>
+                <h3 className="m-0">Horse</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="cisneswan()">
+                <img src="../imagens/animais/cisne-swan.png" className="d-block m-auto"/>
+                <h2 className="m-0">Cisne</h2>
+                <h3 className="m-0">Swan</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="cobracascavelrattlesnake()">
+                <img src="../imagens/animais/cobra-cascavel-rattlesnake.png" className="d-block m-auto"/>
+                <h2 className="m-0">Cobra Cascavel</h2>
+                <h3 className="m-0">Rattlesnake</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="coelhorabbit()">
+                <img src="../imagens/animais/coelho-rabbit.png" className="d-block m-auto"/>
+                <h2 className="m-0">Coelho</h2>
+                <h3 className="m-0">Rabbit</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="corujaowl()">
+                <img src="../imagens/animais/coruja-owl.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Coruja</h2>
+                <h3 className="m-0">Owl</h3>
+            </article>
+            
+            
+            
+            <article className="animal lilas" onclick="corvocrow()">
+                <img src="../imagens/animais/corvo-crow.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Corvo</h2>
+                <h3 className="m-0">Crow</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="dinossaurodinosaur()">
+                <img src="../imagens/animais/dinossauro-dinosaur.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Dinossauro</h2>
+                <h3 className="m-0">Dinosaur</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="dragaodekomodokomododragon()">
+                <img src="../imagens/animais/dragao-de-komodo-komodo-dragon.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Dragão de Komodo</h2>
+                <h3 className="m-0">Komodo Dragon</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="elefanteelephant()">
+                <img src="../imagens/animais/elefante-elephant.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Elefante</h2>
+                <h3 className="m-0">Elephant</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="escorpiaoscorpion()">
+                <img src="../imagens/animais/escorpiao-scorpion.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Escorpião</h2>
+                <h3 className="m-0">Scorpion</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="falcaofalcon()">
+                <img src="../imagens/animais/falcao-falcon.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Falcão</h2>
+                <h3 className="m-0">Falcon</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="furaoferret()">
+                <img src="../imagens/animais/furao-ferret.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Furão</h2>
+                <h3 className="m-0">Ferret</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="galinhachicken()">
+                <img src="../imagens/animais/galinha-chicken.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Galinha</h2>
+                <h3 className="m-0">Chicken</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="galorooster()">
+                <img src="../imagens/animais/galo-rooster.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Galo</h2>
+                <h3 className="m-0">Rooster</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="gatocat()">
+                <img src="../imagens/animais/gato-cat.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Gato</h2>
+                <h3 className="m-0">Cat</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="girafagiraffe()">
+                <img src="../imagens/animais/girafa-giraffe.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Girafa</h2>
+                <h3 className="m-0">Giraffe</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="guaxinimraccoon()">
+                <img src="../imagens/animais/guaxinim-raccoon.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Guaxinim</h2>
+                <h3 className="m-0">Raccoon</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="hienahyena()">
+                <img src="../imagens/animais/hiena-hyena.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Hiena</h2>
+                <h3 className="m-0">Hyena</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="hipopotamohippopotamus()">
+                <img src="../imagens/animais/hipopotamo-hippopotamus.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Hipopótamo</h2>
+                <h3 className="m-0">Hippopotamus</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="iaqueyak()">
+                <img src="../imagens/animais/iaque-yak.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Iaque</h2>
+                <h3 className="m-0">Yak</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="jacarealligator()">
+                <img src="../imagens/animais/jacare-alligator.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Jacaré</h2>
+                <h3 className="m-0">Alligator</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="lagartolizard()">
+                <img src="../imagens/animais/lagarto-lizard.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Lagarto</h2>
+                <h3 className="m-0">Lizard</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="leaolion()">
+                <img src="../imagens/animais/leao-lion.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Leão</h2>
+                <h3 className="m-0">Lion</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="leopardoleopard()">
+                <img src="../imagens/animais/leopardo-leopard.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Leopardo</h2>
+                <h3 className="m-0">Leopard</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="lobowolf()">
+                <img src="../imagens/animais/lobo-wolf.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Lobo</h2>
+                <h3 className="m-0">Wolf</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="lontraotter()">
+                <img src="../imagens/animais/lontra-otter.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Lontra</h2>
+                <h3 className="m-0">Otter</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="macacoape()">
+                <img src="../imagens/animais/macaco-ape.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Macaco</h2>
+                <h3 className="m-0">Ape</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="morcegobat()">
+                <img src="../imagens/animais/morcego-bat.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Morcego</h2>
+                <h3 className="m-0">Bat</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="ovelhasheep()">
+                <img src="../imagens/animais/ovelha-sheep.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Ovelha</h2>
+                <h3 className="m-0">Sheep</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="panda()">
+                <img src="../imagens/animais/panda.png" classNameName="d-block m-auto"/>
+                <h2 className="m-0">Panda</h2>
+                <h3 className="m-0">Panda</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="papagaiocinzentoafricangreyparrot()">
+                <img src="../imagens/animais/papagaio-cinzento-african-grey-parrot.png" className="d-block m-auto"/>
+                <h2 className="m-0">Papagaio Cinza</h2>
+                <h3 className="m-0">African Grey Parrot</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="patoduck()">
+                <img src="../imagens/animais/pato-duck.png" className="d-block m-auto"/>
+                <h2 className="m-0">Pato</h2>
+                <h3 className="m-0">Duck</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="peruturkey()">
+                <img src="../imagens/animais/peru-turkey.png" className="d-block m-auto"/>
+                <h2 className="m-0">Peru</h2>
+                <h3 className="m-0">Turkey</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="pinguimpenguin()">
+                <img src="../imagens/animais/pinguim-penguin.png" className="d-block m-auto"/>
+                <h2 className="m-0">Pinguim</h2>
+                <h3 className="m-0">Penguin</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="piscorobin()">
+                <img src="../imagens/animais/pisco-robin.png" className="d-block m-auto"/>
+                <h2 className="m-0">Pisco</h2>
+                <h3 className="m-0">Robin</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="pombadove()">
+                <img src="../imagens/animais/pomba-dove.png" className="d-block m-auto"/>
+                <h2 className="m-0">Pomba</h2>
+                <h3 className="m-0">Dove</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="porcopig()">
+                <img src="../imagens/animais/porco-pig.png" className="d-block m-auto"/>
+                <h2 className="m-0">Porco</h2>
+                <h3 className="m-0">Pig</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="porcodaindiaguineapig()">
+                <img src="../imagens/animais/porco-da-india-guinea-pig.png" className="d-block m-auto"/>
+                <h2 className="m-0">Porco da India</h2>
+                <h3 className="m-0">Guinea Pig</h3>
+            </article>
+             
+            
+            
+            <article className="animal amarelo" onclick="porcoespinhohedgehog()">
+                <img src="../imagens/animais/porco-espinho-hedgehog.png" className="d-block m-auto"/>
+                <h2 className="m-0">Porco Espinho</h2>
+                <h3 className="m-0">Hedgehog</h3>
+            </article>
+             
+            
+            
+            <article className="animal rosa" onclick="ratorat()">
+                <img src="../imagens/animais/rato-rat.png" className="d-block m-auto"/>
+                <h2 className="m-0">Rato</h2>
+                <h3 className="m-0">Rat</h3>
+            </article>
+             
+            
+            
+            <article className="animal vermelho" onclick="rinoceronterhinoceros()">
+                <img src="../imagens/animais/rinoceronte-rhinoceros.png" className="d-block m-auto"/>
+                <h2 className="m-0">Rinoceronte</h2>
+                <h3 className="m-0">Rhinoceros</h3>
+            </article>
+             
+            
+            
+            <article className="animal lilas" onclick="sapofrog()">
+                <img src="../imagens/animais/sapo-frog.png" className="d-block m-auto"/>
+                <h2 className="m-0">Sapo</h2>
+                <h3 className="m-0">Frog</h3>
+            </article>
+             
+            
+            
+            <article className="animal azul" onclick="tamanduaanteater()">
+                <img src="../imagens/animais/tamandua-anteater.png" className="d-block m-auto"/>
+                <h2 className="m-0">Tamanduá</h2>
+                <h3 className="m-0">Anteater</h3>
+            </article>
+             
+            
+            
+            <article className="animal verde" onclick="tigretiger()">
+                <img src="../imagens/animais/tigre-tiger.png" className="d-block m-auto"/>
+                <h2 className="m-0">Tigre</h2>
+                <h3 className="m-0">Tiger</h3>
+            </article>            
+
+             
+        </section>
+       
+    
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    
+       
+    </>
+  );
+};
+
+export default MeuComponenteHTML;
