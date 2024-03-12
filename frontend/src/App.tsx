480251177fda3cfb06e40bcdf2fabd85eb73a5b2
Alysson <limalysson@gmail.com>
2024-03-11 21:21:32 -0300

    Criando Componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
-import { Tooltip, Toast, Popover } from 'bootstrap';
+import Animal from './components/Animal'
+
 
 function App() {
   return (
     <>
-      <button id="popoverButton" type="button"
-        className="btn btn-primary btn-lg"
-        data-bs-toggle="popover" title="ESM in Browser"
-        data-bs-content="Bang!">Custom popover</button>
+      <Animal/>
     </>
   )
 }
